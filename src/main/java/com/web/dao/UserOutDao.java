6a55a2fb6a2240f5c01970142336c81c3893f830
sukey <ymyangshukai@126.com>
2016-06-29 20:45:11 +0800

    登陆注册模块接口完成

--- src/main/java/com/web/dao/UserOutDao.java
+++ src/main/java/com/web/dao/UserOutDao.java
@@ -14,11 +14,14 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class UserOutDao extends BaseDao {
 
-    public List findUser( USER_OUT user ) {
+    public List findUser(USER_OUT user) {
         StringBuffer sql = new StringBuffer();
         sql.append("From USER_OUT u where 1=1");
         if (user.getUsername() != null && !"".equals(user.getUsername())) {
             sql.append(" and u.username =:username");
+        }
+        if (user.getMobile_phone() != null && !"".equals(user.getContact_phone())) {
+            sql.append(" and u.mobile_phone =:mobile_phone");
         }
 
         return super.findObjects(sql.toString(), user);
---
+++ src/main/java/com/web/service/VerifyCodeService.java
@@ -0,0 +1,40 @@
+package com.web.service;
+
+import java.util.List;
+
+import com.web.dao.VerifyCodeDao;
+import com.web.entity.VerifyCode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by sukey on 2016/6/28.
+ */
+
+@Service
+public class VerifyCodeService {
+
+    @Autowired
+    private VerifyCodeDao verifyCodeDao;
+
+    /**
+     * 根据电话号码、验证码查询，验证码是否存在
+     * @param phone
+     * @param code
+     * @return
+     */
+    public VerifyCode findVerifyCode(String phone,String code) {
+        VerifyCode verifyCode =new VerifyCode();
+        verifyCode.setPhone(phone);
+        verifyCode.setCode(code);
+        List<VerifyCode> verifyCodeList=verifyCodeDao.findVerifyCode(verifyCode);
+        if(verifyCodeList.size()==1){
+            return verifyCodeList.get(0);
+        }
+        return null;
+    }
+
+    public void saveVerifyCode(VerifyCode verifyCode){
+        verifyCodeDao.save(verifyCode);
+    }
+}
