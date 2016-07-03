7c18c1fd3b107526e781195ec220c9e2f798b89d
sukey <ymyangshukai@126.com>
2016-07-03 17:01:25 +0800

    添加监管用户

---
+++ src/main/java/com/web/Exception/UserNameExistException.java
@@ -0,0 +1,11 @@
+package com.web.Exception;
+
+/**
+ * Created by sukey on 2016/7/3.
+ */
+public class UserNameExistException extends RuntimeException {
+
+    public UserNameExistException() {
+        super("该昵称已存在");
+    }
+}
